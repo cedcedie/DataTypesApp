7c0de6b4e50d8ef96cbccf6b2c89f8785e3bd9d1
cedri <cedri@CEDRICJAMES>
2024-02-23 07:47:25 +0800

    Add project files.

---
+++ DataTypesApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace DataTypesApp
+{
+    class DataTypesProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the pieces of apple : ");
+            int apple = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the total price of the apple(s): ");
+            double totalPrice = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Total price for {apple} piece(s) of apple: {totalPrice}");
+
+            int convertedPrice = (int)totalPrice;
+
+            Console.WriteLine($"Original price: {totalPrice}");
+            Console.WriteLine($"Converted price: {convertedPrice}");
+
+        }
+    }
+}
